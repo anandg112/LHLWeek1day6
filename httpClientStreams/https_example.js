e772c28dd43139ea88e51e9d03e1154798eb8e1d
anandg112 <anandg112@gmail.com>
2017-02-12 15:38:27 +0000

    issuing a GET request for script to Monty Python using https module

--- httpClientStreams/https_example.js
+++ httpClientStreams/https_example.js
@@ -3,12 +3,18 @@ var https = require("https");
 console.log('I did it!');
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
-var callback = function() {
+var callback = function(response) {
   console.log("In response handler callback!");
+
+  response.on('data', function(chunk){
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]')
+  });
+
+  //console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request");
